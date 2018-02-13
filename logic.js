8e73e421aedbf364e6564848fb9917df694eefd7
Vishvesh Dave <vishvesh01@yahoo.com>
2018-02-13 14:23:09 -0500

    added all links

--- logic.js
+++ logic.js
@@ -1,9 +1,11 @@
-function onClick(element) {
-	document.getElementById("img01").src = element.src;
-	document.getElementById("modal01").style.display = "block";
-	var captionText = document.getElementById("caption");
-	captionText.innerHTML = element.alt;
-}
+//
+// // Modal Image Gallery
+// function onClick(element) {
+// 	document.getElementById("img01").src = element.src;
+// 	document.getElementById("modal01").style.display = "block";
+// 	var captionText = document.getElementById("caption");
+// 	captionText.innerHTML = element.alt;
+// }
 
 // Change style of navbar on scroll
 window.onscroll = function() {myFunction()};
@@ -19,7 +21,7 @@ function myFunction() {
 // Used to toggle the menu on small screens when clicking on the menu button
 function toggleFunction() {
 	var x = document.getElementById("navDemo");
-	if (x.className.indexOf("w3-show") === -1) {
+	if (x.className.indexOf("w3-show") == -1) {
 		x.className += " w3-show";
 	} else {
 		x.className = x.className.replace(" w3-show", "");
